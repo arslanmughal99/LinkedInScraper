f69b8f34ef2308f10c84e0e22cc89b1f5b640ffb
arslanmughal99 <arslanmughal5566@outlook.com>
2021-05-01 20:54:07 +0500

    Working to add country support

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -7,22 +7,22 @@ import { ResultsQueueService } from './results-queue.service';
 
 @Module({
   imports: [
-    HttpModule,
-    // HttpModule.registerAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (configs: ConfigService) => ({
-    //     proxy: {
-    //       host: configs.get('PROXY_HOST'),
-    //       protocol: configs.get('PROXY_TYPE'),
-    //       port: parseInt(configs.get('PROXY_PORT')),
-    //       auth: {
-    //         username: configs.get('PROXY_USERNAME'),
-    //         password: configs.get('PROXY_PASSWORD'),
-    //       },
-    //     },
-    //   }),
-    // }),
+    // HttpModule,
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configs: ConfigService) => ({
+        proxy: {
+          host: configs.get('PROXY_HOST'),
+          protocol: configs.get('PROXY_TYPE'),
+          port: parseInt(configs.get('PROXY_PORT')),
+          auth: {
+            username: configs.get('PROXY_USERNAME'),
+            password: configs.get('PROXY_PASSWORD'),
+          },
+        },
+      }),
+    }),
   ],
   providers: [ScraperService, ResultsQueueService],
   controllers: [ScraperController],
